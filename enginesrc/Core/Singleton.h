4c5cd6872cbe7b5b485a19d1f91a8710a7c66f94
Luminouss <luminouscsgo@gmail.com>
2018-01-01 21:04:57 +0100

    Added Documentation

--- enginesrc/Core/Singleton.h
+++ enginesrc/Core/Singleton.h
@@ -10,31 +10,46 @@ namespace le{
 		template <typename T>
 		class Singleton{
 		public:
-
+			/**
+			* Creates a Singleton instance of a specific Type
+			*@return true if instance was created, false if instance was already instantiated
+			*/
 			static bool create( )
 			{
 				if( Singleton::m_instance != nullptr )
 					return false;
 
 				Singleton::m_instance = new T( );
+				return true;
 			}
-
-			static T* create( T* in )
+			/**
+			* Creates a Singleton instance of a specific with a specified Pointer 
+			* @param Pointer that the Singleton should use to be instantiated
+			* @return true if instance was created, false if instance was already instantiated
+			*/
+			static bool create( T* in )
 			{
 
 				if( Singleton::m_instance != nullptr )
 					return false;
 
 				Singleton::m_instance = in;
+				return true;
 			}
-
+			/**
+			*Gets the Singleton pointer with the specific Type
+			@return T* The singleton to be returned
+			*/
 			static T* get( )
 			{
 				assert( Singleton::m_instance != nullptr );
 				return Singleton::m_instance;
 			}
 
-
+			/**
+			* Tries to destroy the Singleton instance if it was already instantiated
+			* @return true if instance was destroyed, false if instance hasn't even been instantiated
+			*/
 			static bool destroy( )
 			{
 				if( Singleton::m_instance == nullptr )
@@ -45,7 +60,8 @@ namespace le{
 			}
 
 		private:
-			static T* m_instance;
+			
+			static T* m_instance;/** Pointer of Singleton instance */
 #pragma region Constructors
 			Singleton( ) { }
 			~Singleton( ) { }
--- enginesrc/IO/Log.h
+++ enginesrc/IO/Log.h
@@ -8,13 +8,25 @@
 #define ll_log_if(A,B)le::core::Singleton<le::io::Log>::get()->log_if(A,B)
 #else
 #define ll_log(A) 
+#define ll_log_if(A,B)
 #endif
 
 namespace le{
 	namespace io{
 		class Log{
 		public:
+			/**
+			* Logs the specified String 
+			* @param String to be logged
+			* @return void
+			*/
 			void log( std::string str );
+			/**
+			* Logs the specified String if a certain Expression evaluates to true
+			* @param String to be logged
+			* @param Expression to be checked
+			* @return void
+			*/
 			void log_if( std::string str, bool expr );
 		};
 	}
