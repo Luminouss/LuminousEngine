3c4e01bbb33da43be5247dc20f95ddbbe52f2c22
Luminouss <luminouscsgo@gmail.com>
2018-01-03 21:07:27 +0100

    added readRegion/writeRegion

--- enginesrc/IO/FileSystem.cpp
+++ enginesrc/IO/FileSystem.cpp
@@ -4,7 +4,7 @@
 
 namespace le{
 	namespace io{
-		bool FileSystem::copyFile( const std::string& path, const std::string& outPath , const bool overwrite)
+		bool FileSystem::copyFile( const std::string& path, const std::string& outPath, const bool overwrite )
 		{
 			//Todo: Error handling
 			if( fileExist( path ) ){
@@ -16,7 +16,7 @@ namespace le{
 						return false;
 					}
 				} else{
-					boost::filesystem::copy_file( path, outPath);
+					boost::filesystem::copy_file( path, outPath );
 					return true;
 				}
 			} else{
@@ -46,19 +46,45 @@ namespace le{
 			boost::filesystem::ifstream ifs{ p };
 			std::string temp;
 			while( getline( ifs, temp ) )
-				outStr += temp;
+				outStr += temp + '\n';
 			return true;
 		}
 
-		std::string FileSystem::readFile( const std::string& path)
+		std::string FileSystem::readFile( const std::string& path )
 		{
 			//Todo: Error Handling
 			boost::filesystem::path p{ path };
 			boost::filesystem::ifstream ifs{ p };
-			std::string temp,a;
+			std::string temp, a;
 			while( getline( ifs, a ) )
-				temp += a;
+				temp += a + '\n';;
 			return temp;
+		}
+
+		bool FileSystem::readFileRegionF( const std::string & path, std::string & outStr, int begin )
+		{
+			outStr = readFile( path ).substr( begin );
+			//Todo: Error Handling
+			return true;
+		}
+
+		std::string FileSystem::readFileRegionF( const std::string & path, int begin )
+		{
+
+			return readFile( path ).substr( begin );
+		}
+
+		bool FileSystem::readFileRegionFT( const std::string & path, std::string & outStr, int begin, int len )
+		{
+
+			outStr = readFile( path ).substr( begin, len );
+			//Todo: Error Handling
+			return true;
+		}
+
+		std::string FileSystem::readFileRegionFT( const std::string & path, int begin, int len )
+		{
+			return readFile( path ).substr( begin, len );
 		}
 
 		bool FileSystem::writeFile( const std::string& path, const std::string& inStr )
@@ -69,6 +95,26 @@ namespace le{
 			std::string temp = readFile( path );
 			boost::filesystem::ofstream ofs{ p };
 			ofs << temp << inStr;
+			return true;
+		}
+		bool FileSystem::overwriteFile( const std::string & path, const std::string & inStr )
+		{
+
+			//Todo: Error Handling
+			//Todo: Add Overwrite Handling
+			boost::filesystem::path p{ path };
+			boost::filesystem::ofstream ofs{ p };
+			ofs <<inStr;
+			return true;
+		}
+		bool FileSystem::writeFileOffset( const std::string & path, const std::string & inStr, int offset )
+		{
+			//Todo: Error Handling
+			//Todo: Add Overwrite Handling
+			boost::filesystem::path p{ path };
+			std::string temp = readFileRegionFT( path ,0,offset) + inStr + readFileRegionF(path,offset);
+			boost::filesystem::ofstream ofs{ p };
+			ofs << temp;
 			return true;
 		}
 	}
--- enginesrc/IO/FileSystem.h
+++ enginesrc/IO/FileSystem.h
@@ -11,15 +11,21 @@
 
 #define lfs_readFile(A,B) le::core::Singleton<le::io::FileSystem>::get()->readFile(A,B)
 #define lfs_readFile(A) le::core::Singleton<le::io::FileSystem>::get()->readFile(A)
+#define lfs_readFileF(A,B,C) le::core::Singleton<le::io::FileSystem>::get()->readFileRegionF(A,B,C)
+#define lfs_readFileF(A,B) le::core::Singleton<le::io::FileSystem>::get()->readFileRegionF(A,B)
+#define lfs_readFileFT(A,B,C,D) le::core::Singleton<le::io::FileSystem>::get()->readFileRegionFT(A,B,C,D)
+#define lfs_readFileFT(A,B,C) le::core::Singleton<le::io::FileSystem>::get()->readFileRegionFT(A,B,C)
 
 #define lfs_writeFile(A,B) le::core::Singleton<le::io::FileSystem>::get()->writeFile(A,B)
+#define lfs_overwriteFile(A,B) le::core::Singleton<le::io::FileSystem>::get()->overwriteFile(A,B)
+#define lfs_writeFileOff(A,B,C) le::core::Singleton<le::io::FileSystem>::get()->writeFileOffset(A,B,C)
 
 namespace le{
 	namespace io{
 
 		class FileSystem{
-			//read (from-to)	(/)
-			//write (from-to)	(/)
+			//read (from-to)	(X)
+			//write (offset)	(X)
 			//copy				(X)
 			//delete			(X)
 			//check if exists	(X)
@@ -65,6 +71,42 @@ namespace le{
 			std::string readFile( const std::string& path );
 
 			/**
+			* Read a specific Region of the File
+			* @param Path to the File which should be read from
+			* @param Reference to the String which will contain the content of the File
+			* @param Beginning Offset of the Region to be read
+			* @return true if File was read correctly, false if a Error occured
+			*/
+			bool readFileRegionF( const std::string& path, std::string& outStr, int begin);
+
+			/**
+			* Read a specific Region of the File
+			* @param Path to the File which should be read from
+			* @param Beginning Offset of the Region to be read
+			* @return The String that has been read
+			*/
+			std::string readFileRegionF( const std::string& path, int begin);
+
+			/**
+			* Read a specific Region of the File
+			* @param Path to the File which should be read from
+			* @param Reference to the String which will contain the content of the File
+			* @param Beginning Offset of the Region to be read
+			* @param Amount of Characters to be read
+			* @return true if File was read correctly, false if a Error occured
+			*/
+			bool readFileRegionFT( const std::string& path, std::string& outStr,int begin,int len );
+
+			/**
+			* Read a specific Region of the File
+			* @param Path to the File which should be read from
+			* @param Beginning Offset of the Region to be read
+			* @param Amount of Characters to be read
+			* @return The String that has been read
+			*/
+			std::string readFileRegionFT( const std::string& path ,int begin, int len);
+
+			/**
 			* Writes a Message into a specified File
 			* @param Path to the File which should be written to
 			* @param String to be written
@@ -72,6 +114,22 @@ namespace le{
 			*/
 			bool writeFile( const std::string& path, const std::string& inStr );
 
+			/**
+			* Overwrites a specified File
+			* @param Path to the File which should be written to
+			* @param String to be written
+			* @return true if File was written correctly, false if a Error occured
+			*/
+			bool overwriteFile( const std::string& path, const std::string& inStr );
+			
+			/**
+			* Writes a Message into a specified File with an offset from the start
+			* @param Path to the File which should be written to
+			* @param String to be written
+			* @param Offset for the written stuff
+			* @return true if File was written correctly, false if a Error occured
+			*/
+			bool writeFileOffset( const std::string& path, const std::string& inStr ,int offset);
 		};
 	}
 }
