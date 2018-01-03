94e863f51267136fb32f3970503bb478ad1ba147
Luminouss <luminouscsgo@gmail.com>
2018-01-03 21:07:09 +0100

    Small File to manipulate Strings
    -split function

---
+++ enginesrc/Helpers/StringManipulator.h
@@ -0,0 +1,31 @@
+#ifndef STRINGMANIPULATOR_H
+#define STRINGMANIPULATOR_H
+
+#include <string>
+#include <vector>
+
+namespace le{
+	namespace help{
+		class StringManipulator{
+		public:
+			static std::vector<std::string> splitString( std::string splitString, const char splitter )
+			{ 
+				//Todo: fix and cleanup
+				std::vector<std::string> temp;
+				int splitpos = splitString.find( splitter );
+				while( true ){
+					if( splitpos == -1 ){
+						temp.push_back( splitString );
+						return temp;
+					} else{
+						temp.push_back( splitString.substr( 0, splitpos ) );
+						splitString.erase( splitString.begin( ), splitString.begin( ) + splitpos +1 );
+						splitpos = splitString.find( splitter );
+					}
+				}
+			}
+		};
+	}
+}
+
+#endif
