d5eccf350c2bdc1e41eb98eddd5636dba7588fd0
Luminouss <luminouscsgo@gmail.com>
2018-01-05 23:02:31 +0100

    Console in Windows Subsystem
    Now able to load the Console while in the Windows Subsystem and get the handles to the io

--- enginesrc/IO/Log.cpp
+++ enginesrc/IO/Log.cpp
@@ -2,6 +2,26 @@
 #include <iostream>
 namespace le{
 	namespace io{
+		void Log::Initialize( )
+		{
+			m_fstreamin= nullptr;
+			m_fstreamout = nullptr;
+			if( AllocConsole() ==NULL ){
+				return;
+			}
+
+			freopen_s( &m_fstreamin, "CONIN$", "r", stdin );
+			freopen_s( &m_fstreamout, "CONOUT$", "w", stdout );
+
+			m_hConsoleInput = GetStdHandle( STD_INPUT_HANDLE );
+			m_hConsoleOutput = GetStdHandle( STD_OUTPUT_HANDLE );
+			m_consoleWindow = GetConsoleWindow( );
+
+			if( m_fstreamin == nullptr || m_fstreamout == nullptr || !m_hConsoleInput || !m_hConsoleOutput || !m_consoleWindow ){
+				return;
+			}
+
+		}
 		void Log::log_if( const std::string str, const bool expr )
 		{
 			if( expr )
--- enginesrc/IO/Log.h
+++ enginesrc/IO/Log.h
@@ -2,10 +2,17 @@
 #define LOG_H
 
 #include <string>
+#include <windows.h>
+#include <fstream>
+#include <iostream>
 
 #ifdef _DEBUG
 #define ll_log(A) le::core::Singleton<le::io::Log>::get()->log(A)
 #define ll_log_if(A,B) le::core::Singleton<le::io::Log>::get()->log_if(A,B)
+
+//Todo: ;^)
+#define ll_err(A) le::core::Singleton<le::io::Log>::get()->log(A)
+#define ll_err_if(A,B) le::core::Singleton<le::io::Log>::get()->log_if(A,B)
 #else
 #define ll_log(A) 
 #define ll_log_if(A,B)
@@ -15,6 +22,12 @@ namespace le{
 	namespace io{
 		class Log{
 		public:
+			/**
+			* Initializes the Console Window and loads the Handles
+			* @return void
+			*/
+			void Initialize( );
+
 			/**
 			* Logs the specified String 
 			* @param String to be logged
@@ -28,6 +41,11 @@ namespace le{
 			* @return void
 			*/
 			void log_if( const std::string str, const bool expr );
+		private:
+			FILE* m_fstreamin;
+			FILE* m_fstreamout;
+			HANDLE m_hConsoleInput, m_hConsoleOutput;
+			HWND m_consoleWindow;
 		};
 	}
 }
